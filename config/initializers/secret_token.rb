47edc3fcfd492b2ee3ec9e7592ce30b1f8512765
Luan fonseca de farias <luanfonceca@gmail.com>
2013-05-28 14:13:46 -0300

    First commit :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GerConsult::Application.config.secret_token = 'eb85d24b4be610f4cb458dd91e35c262906eb82655bb6c2c84ee2d66ff59e1f1bec62831c619eb1b3e86d0134008862ccc421f7ea8dfbd17e1b2ab9a0f998126'
